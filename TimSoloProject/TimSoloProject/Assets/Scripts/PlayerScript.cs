420427b6cdda40fb7d80b9154bed05a771dca5b1
LAPTOP-RR70KLG2\ezio9 <tpreble@mail.pima.edu>
2017-09-09 01:10:39 -0700

    no message

--- TimSoloProject/TimSoloProject/Assets/Scripts/PlayerScript.cs
+++ TimSoloProject/TimSoloProject/Assets/Scripts/PlayerScript.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        
+    // Use this for initialization
+    void Start () {
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
-        {
 
+        //The longer the button is held, the longer the distance you will go (to a point).
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Hello! This is what happens when you hold the spacebar.");
         }
 	}
 }
